b8e97edddfd953ba3f69f9f1c49df4a2e0373c65
Sunil Kumar C <sunil@Sunils-MacBook-Air.local>
2020-08-02 21:12:49 +0530

    Season

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class App extends React.Component {
+  state = {
+    lat: null,
+    errorMsg: "",
+  };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (error) => this.setState({ errorMsg: error.message })
+    );
+  }
+
+  render() {
+    if (this.state.lat && !this.state.errorMsg) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+    if (this.state.errorMsg && !this.state.lat) {
+      return <div>Error: {this.state.errorMsg}</div>;
+    }
+    return <div>Loading...</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
